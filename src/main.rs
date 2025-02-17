cde736ceccab5206ee89d186577dd86d681b60f3
Bart Massey <bart.massey@gmail.com>
2025-02-17 08:30:35 -0800

    brought in more of chapter 04

--- src/main.rs
+++ src/main.rs
@@ -119,6 +119,7 @@ mod console;
 mod cpu;
 mod panic_wait;
 mod print;
+mod synchronization;
 
 /// Early init code.
 ///
@@ -126,7 +127,12 @@ mod print;
 ///
 /// - Only a single core must be active and running this function.
 unsafe fn kernel_init() -> ! {
-    println!("Hello from Rust!");
+    use console::console;
 
-    panic!("Stopping here.")
+    println!("[0] Hello from Rust!");
+
+    println!("[1] Chars written: {}", console().chars_written());
+
+    println!("[2] Stopping here.");
+    cpu::wait_forever()
 }
